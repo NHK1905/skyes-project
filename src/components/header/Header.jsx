bf3071c4b629e85e00ef706b88ed766e9fe2ac67
nhk1905 <nguyenhuykhang1905@gmail.com>
2022-10-16 04:26:17 +0700

    Skyes Project Day 1

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './header.scss'
+import { logo } from '../../assets/images'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header__inner container">
+            <div className="header__logo">
+                <img src={logo} alt="" />
+            </div>
+            <ul className="header__nav">
+                <li><a href="#">About Skyes</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Champions</a></li>
+                <li><a href="#">Support</a></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Header
