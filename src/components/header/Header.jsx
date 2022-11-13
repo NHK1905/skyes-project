c0eba394e40ee5669d110d98e4fa3fe4018834a0
nhk1905 <nguyenhuykhang1905@gmail.com>
2022-11-13 18:44:47 +0700

    New Update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,12 +7,18 @@ const Header = () => {
     <div className="header">
         <div className="header__inner container">
             <div className="header__logo">
-                <img src={logo} alt="" />
+              <div class="header__text">
+                <div className="header__text__neon">
+                    <div className="header__text__title">SKYES</div>
+                    <div className="header__text__gradient"></div>
+                    <div className="header__text__dodge"></div>
+                </div>
+              </div>
             </div>
             <ul className="header__nav">
                 <li><a href="#">About Skyes</a></li>
                 <li><a href="#">News</a></li>
-                <li><a href="#">Champions</a></li>
+                <li><a href="#">Modal</a></li>
                 <li><a href="#">Support</a></li>
             </ul>
         </div>
